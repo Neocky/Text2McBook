c6445cf0a386dd0e1b1b45e0e432bda75507acf7
Neocky <Neocky@users.noreply.github.com>
2024-02-23 01:25:56 +0100

    Added different book formats

--- README.md
+++ README.md
@@ -1,25 +1,116 @@
-# Text2McBook
-📚 Convert any textfile to a formatted text for minecraft books
 
-## Issues? Found a bug? 
+# Text2McBook
+
+📚 Convert any textfile to a formatted text for minecraft books to use them as easily as possible ingame.
+
+## Issues? Found a bug?
+
 [Create an issue.](https://github.com/Neocky/Text2McBook/issues/new/choose) 
 
 ## About
-This script will convert a text file and format the text in a way in which it can be easily copy & pasted in a book in the game.  
+
+This script will convert a text file and output a formatted file with the text in a way in which it can be easily used ingame like with the `/give` command.
+It supports different book formats which can be choosen with the parameter [`-f FORMATNAME`]().
+
+### Default
+
+The `default` format will create a `/give` command which can be easily copied into a command block ingame to get the writable book.
+It will also format the text in a way in which it can be easily copy and pasted in a book in the game. 
+
+### Denizen
+
+[Denizen](https://denizenscript.com/) is a scripting plugin for minecraft servers.  
+The `denizen` format will format the text for the `text` property in [Book Script Containers](https://meta.denizenscript.com/Docs/Search/book#book%20script%20containers). It will replace new line symbols with `<n>` and indents the line with 4 spaces to make it easily pasteable in the `text` properties of `book script containers`.
+
+
 I got inspiration from a post from [r/admincraft](https://www.reddit.com/r/admincraft/)  
 
 ## Installation
+
 ### Python
-Python needs to be installed on your machine.  
-Get it [here](https://www.python.org/downloads/).  
+
+Python (minimum: Version 3.10.4) needs to be installed on your machine.  
+Get it [here](https://www.python.org/downloads/).
+
 ### Clone the repository / Download the zip
+
 Download the script:
 Clone the repository or get the latest release [here](https://github.com/Neocky/Text2McBook/releases).
 
 ## Usage
-Create/edit the textfile in the same folder where the python script is with the name: `text.txt`  
-Add your text that you want to convert in this file.  
-This is the text that you want to be converted to a minecraft book format so you can easily copy paste in a book ingame.  
-Run the script: `text2mcbook.py`  
+
+Create/edit the textfile in the same folder where the python script is with the name: `text.txt` and add your book text in there.  
+This is the text that you want to be converted to a minecraft book format.  
+Run the script: `text2mcbook.py`.  
 Now a `booktext.txt` file should be created.  
-In there is the formated text which you can easily copy & paste in a book in minecraft.
+In there is a `/give` command which can be inputted into a command block to get the book. There is also the formated text which you can easily copy & paste in a book in minecraft.
+
+
+## Advanced Usage
+
+### Parmaters
+
+This script supports the following parameters to change the usage for the script.
+
+#### Get help about all available parameters
+
+```shell
+py text2mcbook.py -h
+```
+
+#### Input file
+
+```shell
+py text2mcbook.py INPUTFILENAME
+```
+
+Default: `text.txt`  
+Changes the input file.
+
+#### Output file
+
+```shell
+py text2mcbook.py INPUTFILENAME OUTPUTFILENAME
+```
+
+Default: `booktext.txt`  
+Changes the output file.
+
+#### Book format
+
+```shell
+py text2mcbook.py -f FORMAT
+py text2mcbook.py --format FORMAT
+```
+
+Default: `default`  
+Possible Values: `default`, `denizen`  
+Changes the book format and the resulting formatting.
+
+#### Characters per line
+
+```shell
+py text2mcbook.py -c NUMBEROFCHARACTERSPERLINE
+py text2mcbook.py --characterlimit NUMBEROFCHARACTERSPERLINE
+```
+
+Default: `19`  
+The number of characters one book line can have.  
+Warning: Don't set it to high or you are risking the loss of words!
+
+### Examples
+
+#### Example 1
+```shell
+py text2mcbook.py book_to_convert.txt book_formatted.txt
+```
+
+Changes the input file to `book_to_convert.txt` and outupts the formatted book text to `book_formatted.txt`.
+
+#### Example 2
+
+```shell
+py text2mcbook.py -f denizen -c 20
+```
+
+Outputs denizen formated text, which can be used in the `text` propert of [Book Script Containers](https://meta.denizenscript.com/Docs/Search/book#book%20script%20containers) in [Denizen](https://denizenscript.com/). Also changes the number of characters per line to 20. 
--- text2mcbook.py
+++ text2mcbook.py
@@ -1,23 +1,84 @@
 import sys
+import argparse
 import textwrap
 
-try:
-    f = open("text.txt", encoding='utf-8') 
-except FileNotFoundError:
-    print("Error: File 'text.txt' wasn't found. Make sure it is in the same directory as this script and run again.")
-    input("Press any key + Enter to end program...")
-    sys.exit()
 
-text = f.read()
-lines = textwrap.wrap(text, 19)
-i = 0
-fi = open("booktext.txt", "w", encoding='utf-8')
-fi.write("Made with Text2McBook  | By Neocky | https://github.com/Neocky/Text2McBook/")
+def main(input_filename, output_filename, format, characters_per_line) -> None:
+    try:
+        with open(input_filename, encoding="utf-8") as f:
+            text = f.read()
 
-for line in range(len(lines)):
-    summ = line % 14
-    if summ == 0:
-        i += 1
-        fi.write("\n")
-        fi.write(f" ### Page: {i} ###\n")
-    fi.write(lines[line] + "\n")
+    except FileNotFoundError as exception:
+        print(f"Error: {type(exception).__name__}")
+        print(f"Error message: {exception}")
+        sys.exit()
+
+    # print warning if characters per line is too high, which could risk in missing words
+    if characters_per_line >= 21:
+        print("Warning: Setting '--charactersperline // -c' too high could result in missing words in books!")
+
+    # split lines every x character 
+    lines = textwrap.wrap(text, characters_per_line)
+    book_pages = []
+    current_book_page = ""
+
+    for line in range(len(lines)):
+        # every page consists of 14 lines
+        line_count = line % 14
+        if line_count == 0 and current_book_page:
+            book_pages.append(current_book_page)
+            current_book_page = ""
+        # create placeholder for linebreaks which gets later replaced
+        current_book_page += lines[line] + "<LINEBREAK>"
+    # print last book page
+    if current_book_page:
+         book_pages.append(current_book_page)
+
+    # create output
+    with open(output_filename, "w", encoding="utf-8") as book_text:
+        book_text.write("Made with Text2McBook by Neocky | https://github.com/Neocky/Text2McBook/\n\n")
+        book_text.write(f"Format: {str(format).lower()}\n")
+        book_text.write(f"Characters per line: {characters_per_line}\n\n")
+        match str(format).lower():
+            # could add more formats here
+            # Denizen | https://denizenscript.com/ | scripting plugin for minecraft servers
+            case "denizen":
+                book_text.write("\n")
+                for current_book_page in book_pages:
+                    # replace new line character with <n> which stands for a new line in a book script container in denizen
+                    # already indent line with 4 spaces to work easily with text property of book container: https://meta.denizenscript.com/Docs/Search/book#book%20script%20containers
+                    current_book_page = current_book_page.replace("<LINEBREAK>", "<n>")
+                    book_text.write(f"    - {current_book_page}\n")
+
+            # Default minecraft book format with give command
+            case _:
+                # add give command for the book
+                # book pages could have less than 14 lines, because we just use a space " " instead of an actual line break symbol here
+                mc_give_command_all_pages = str(book_pages).replace("<LINEBREAK>", " ")
+                mc_give_command = f"/give @p writable_book{{pages:{mc_give_command_all_pages}}} 1"
+                book_text.write(f"Give command:\n{mc_give_command}\n\n")
+
+                # output book in a copy friendly format
+                for i, current_book_page in enumerate(book_pages, start=1):
+                    # replace new line placeholder with actual new line and output to file
+                    current_book_page = current_book_page.replace("<LINEBREAK>", "\n")
+                    book_text.write(f"\n ### Page: {i} ###\n")
+                    book_text.write(f"{current_book_page}\n")
+
+    # check if book could be unsupported
+    if len(book_pages) > 100:
+        print(f"Warning: Book has more than 100 pages ({len(book_pages)})! Using the GUI, a player can write a single book up to 100‌[Java Edition only] or 50‌[Bedrock Edition only] pages long!")
+
+    print(f"Text2McBook convertion of '{input_filename}' with format '{str(format).lower()}' completed! Outputfile: {output_filename}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Arguments for Text2McBook",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("file", nargs="?", help="Filename of textfile which should be converted to minecraft book format", default="text.txt")
+    parser.add_argument("outputfile", nargs="?", help="Filename of output textfile ", default="booktext.txt")
+    parser.add_argument("-f", "--format", nargs="?", help="Book format [default, denizen]", default="default")
+    parser.add_argument("-c", "--characterlimit", nargs="?", type=int, help="Characters per book line", default=19)
+    args = parser.parse_args()
+
+    main(input_filename=args.file, output_filename=args.outputfile, format=args.format, characters_per_line=args.characterlimit)
